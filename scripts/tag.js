06849f996a6a26e8849cea44a1f6e19f51697470
Eugene Hauptmann <eugene@reactivelions.com>
2026-03-06 13:12:12 -0500

    added tag script

---
+++ scripts/tag.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { readFileSync, execSync } from "fs";
+
+const pkg = JSON.parse(readFileSync("package.json", "utf8"));
+const tag = `v${pkg.version}`;
+
+try {
+  execSync(`git tag ${tag}`, { stdio: "inherit" });
+} catch {
+  process.exit(1);
+}
+
+console.log(`\nTag ${tag} created locally.`);
+console.log(`\nTo push it to the remote, run:\n`);
+console.log(`  git push origin ${tag}\n`);
